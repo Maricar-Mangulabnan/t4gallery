37137e8e3e4268671947810ed585f6cc97fdaaa2
Maricar-Mangulabnan <mangulabnanrica3@gmail.com>
2025-01-20 20:00:45 +0800

    Test1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+const mockURLs = [
+    "https://8uo8zvh03s.ufs.sh/f/mSYdP6AH4UtnFmUfrbJmh60doO7SAs13VkPBJwaticzXQGrn",
+    "https://8uo8zvh03s.ufs.sh/f/mSYdP6AH4UtneofFPUUpBOi2SIawAjCxPdsEyUX65ZvntM1g",
+    "https://8uo8zvh03s.ufs.sh/f/mSYdP6AH4UtncWe1KsMxXNqK3sjufP8bUZadMGOF1ErStwz0",
+
+]
+const mockIMAGES = mockURLs.map((url, index) => ({
+  id : index + 1,
+  url,
+
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (Gallery in Progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockIMAGES.map((image) => (
+          <div key= {image.id} className="w-48">
+            <img src={image.url}/>
+              </div>
+        ))}
+      </div>
     </main>
   );
 }
