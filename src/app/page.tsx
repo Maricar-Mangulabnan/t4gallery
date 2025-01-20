6b24b62f6cdcb24729249def10f93fd44e1ffb71
Maricar-Mangulabnan <mangulabnanrica3@gmail.com>
2025-01-20 20:57:27 +0800

    Scaffold UI with Mock Data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 
-const mockURLs = [
+const mockUrls = [
     "https://8uo8zvh03s.ufs.sh/f/mSYdP6AH4UtnFmUfrbJmh60doO7SAs13VkPBJwaticzXQGrn",
     "https://8uo8zvh03s.ufs.sh/f/mSYdP6AH4UtneofFPUUpBOi2SIawAjCxPdsEyUX65ZvntM1g",
     "https://8uo8zvh03s.ufs.sh/f/mSYdP6AH4UtncWe1KsMxXNqK3sjufP8bUZadMGOF1ErStwz0",
 
 ]
-const mockIMAGES = mockURLs.map((url, index) => ({
+const mockIMAGES = mockUrls.map((url, index) => ({
   id : index + 1,
   url,
 
