3b6362251bd08fee96af2cc9cd539037b8ed52b4
Maricar-Mangulabnan <mangulabnanrica3@gmail.com>
2025-02-26 20:32:12 +0800

    error management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,22 @@
 
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Images from "~/app/_components/images";
+import { getMyImages } from "~/server/db/queries";
 
 
-export const dynamic = "force-dynamic"; 
+export const dynamic = "force-dynamic";
 
 
 export default async function HomePage() {
-  
- 
 
   return (
     <main className="">
       <SignedOut>
         <div className="h-full w-full text-2xl text-center">Please Sign In Above  </div>
       </SignedOut>
-      
+
       <SignedIn>
-      <Images/>
+        <Images />
       </SignedIn>
     </main>
   );
--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -1,16 +1,14 @@
-import { auth } from "@clerk/nextjs/server";
 import "server-only";
+import { auth } from "@clerk/nextjs/server";
 import { db } from "./index";
 import { eq } from "drizzle-orm";
 
-
 export async function getMyImages() {
-
-    const user = await auth();
-    if(!user.userId) throw new Error("Unauthorized");
-    const images = await db.query.images.findMany({
-      where: (model) => eq(model.userId, user.userId),
-        orderBy: (model, { desc }) => desc(model.id),
-      });
-    return images;
+  const user = await auth();
+  if (!user.userId) throw new Error("Unauthorized");
+  const images = await db.query.images.findMany({
+    where: (model) => eq(model.userId, user.userId),
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+  return images;
 }
